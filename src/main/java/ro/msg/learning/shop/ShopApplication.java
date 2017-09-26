34e893fd62afea8a71ffec7dc0730b5bd1b89911
Robert Zbiera <robert.zbiera@msg.group>
2017-09-26 06:37:55 +0000

    intelliJ first commit

--- src/main/java/ro/msg/learning/shop/ShopApplication.java
+++ src/main/java/ro/msg/learning/shop/ShopApplication.java
@@ -3,6 +3,11 @@ package ro.msg.learning.shop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * @author Zbiera Alexandru-Robert <Robert.Zbiera@msg.group>
+ * */
+
 @SpringBootApplication
 public class ShopApplication {
 
