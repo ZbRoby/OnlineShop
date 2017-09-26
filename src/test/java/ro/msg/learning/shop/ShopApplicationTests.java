791b13ce7208c53cd33ff9e164ca8a6e9d8c6035
ZbRoby <robi1796@yahoo.com>
2017-09-26 08:15:41 +0000

    added lombok and h2 web console

--- src/test/java/ro/msg/learning/shop/ShopApplicationTests.java
+++ src/test/java/ro/msg/learning/shop/ShopApplicationTests.java
@@ -1,6 +1,5 @@
 package ro.msg.learning.shop;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,9 +12,5 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ShopApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
