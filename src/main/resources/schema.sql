90cf5df4e98aaf14939424cb3a6c8f78f9a9844f
ZbRoby <robi1796@yahoo.com>
2017-09-29 11:32:39 +0300

    moved the schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,4 @@
-DROP TABLE jCategory
-IF EXISTS;
+DROP TABLE jCategory IF EXISTS;
 
 CREATE TABLE jCategory (
     id                INTEGER IDENTITY (1, 1) PRIMARY KEY NOT NULL,
@@ -8,8 +7,7 @@ CREATE TABLE jCategory (
     CONSTRAINT CatToCat FOREIGN KEY (primaryCategoryId) REFERENCES jCategory (id)
 );
 
-DROP TABLE jProducts
-IF EXISTS;
+DROP TABLE jProducts IF EXISTS;
 
 CREATE TABLE jProducts (
     id         INTEGER IDENTITY (1, 1) PRIMARY KEY NOT NULL,
