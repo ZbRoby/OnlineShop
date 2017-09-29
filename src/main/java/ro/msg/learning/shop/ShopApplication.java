0f571eb9a43ffa2bf3e7acf1444b40b918bd2077
ZbRoby <robi1796@yahoo.com>
2017-09-29 09:05:53 +0300

    Added schema.sql to create the tables instead of the ShopApplication's constructor.

--- src/main/java/ro/msg/learning/shop/ShopApplication.java
+++ src/main/java/ro/msg/learning/shop/ShopApplication.java
@@ -1,9 +1,7 @@
 package ro.msg.learning.shop;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  *
@@ -12,26 +10,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class ShopApplication {
-
-    @Autowired
-    public ShopApplication(JdbcTemplate jdbcTemplate) {
-
-        jdbcTemplate.execute("DROP TABLE jCategory IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE jCategory(" +
-            "id INTEGER IDENTITY(1,1) PRIMARY KEY NOT NULL," +
-            "name VARCHAR(255)," +
-            "primaryCategoryId INTEGER," +
-            "CONSTRAINT CatToCat FOREIGN KEY (primaryCategoryId) REFERENCES jCategory(id))");
-
-        jdbcTemplate.execute("DROP TABLE jProducts IF EXISTS");
-        jdbcTemplate.execute("CREATE TABLE jProducts(" +
-            "id INTEGER IDENTITY(1,1) PRIMARY KEY NOT NULL," +
-            "name VARCHAR(255)," +
-            "categoryID INTEGER," +
-            "CONSTRAINT ProdToCat FOREIGN KEY (categoryID) REFERENCES jCategory(id))");
-
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
     }
