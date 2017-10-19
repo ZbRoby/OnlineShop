9dacaed3565c55d9bc69ffeff5d3507dd0819375
ZbRoby <robi1796@yahoo.com>
2017-10-19 13:37:10 +0300

    Added new queries

--- src/main/java/ro/msg/learning/shop/repositories/LocationRepository.java
+++ src/main/java/ro/msg/learning/shop/repositories/LocationRepository.java
@@ -1,6 +1,7 @@
 package ro.msg.learning.shop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ro.msg.learning.shop.entities.Location;
 
@@ -10,4 +11,11 @@ import ro.msg.learning.shop.entities.Location;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
+
+    @Query(value = "SELECT LOCATION_ID FROM PRODUCTS_LOCATIONS WHERE PRODUCT_ID=?1 order by QUANTITY ASC Limit 1", nativeQuery = true)
+    Long findOneLocationIdWithLowestQuantityForProduct(Long idProduct);
+
+    @Query(value = "SELECT LOCATION_ID FROM PRODUCTS_LOCATIONS WHERE PRODUCT_ID=?1 order by QUANTITY DESC Limit 1", nativeQuery = true)
+    Long findOneLocationIdWithHighestQuantityForProduct(Long idProduct);
+
 }
--- src/main/java/ro/msg/learning/shop/repositories/ProductRepository.java
+++ src/main/java/ro/msg/learning/shop/repositories/ProductRepository.java
@@ -1,11 +1,15 @@
 package ro.msg.learning.shop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ro.msg.learning.shop.entities.Product;
+import ro.msg.learning.shop.entities.ProductsLocations;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Zbiera Alexandru-Robert <Robert.Zbiera@msg.group>
@@ -22,5 +26,14 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query(value = "SELECT COALESCE( (SELECT sum(QUANTITY) FROM PRODUCTS_LOCATIONS where PRODUCT_ID=?1 GROUP BY PRODUCT_ID), 0)", nativeQuery = true)
     Long getQuantityForProduct(Long id);
+
+    @Query(value = "select pl from ProductsLocations pl where pl.id.productId in :set")
+    List<ProductsLocations> findAllProductsLocationsInSet(@Param("set") Set<Long> productIdList);
+
+    @Modifying
+    @Query(value = "UPDATE PRODUCTS_LOCATIONS SET QUANTITY=QUANTITY-?3 WHERE LOCATION_ID=?1 AND PRODUCT_ID=?2", nativeQuery = true)
+    void changeTheQuantity(Long locationId, Long productId, Long quantity);
+
+
 
 }
