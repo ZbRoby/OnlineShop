a8064cf968679a02c13440323c49eb7d0947b38a
ZbRoby <robi1796@yahoo.com>
2017-10-23 17:37:40 +0300

    small changes to MostAbundantQS

--- src/main/java/ro/msg/learning/shop/services/strategy/MostAbundantQS.java
+++ src/main/java/ro/msg/learning/shop/services/strategy/MostAbundantQS.java
@@ -2,7 +2,6 @@ package ro.msg.learning.shop.services.strategy;
 
 import ro.msg.learning.shop.entities.ProductsLocations;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
@@ -15,10 +14,7 @@ public class MostAbundantQS implements QuantityStrategy {
 
     @Override
     public List<ProductsLocations> getLocations(Map<Long, Long> productList, List<ProductsLocations> locationAndProduct) {
-        List<ProductsLocations> usedGroup;
-        locationAndProduct.sort(Comparator.comparingLong(ProductsLocations::getQuantity));
-        Collections.reverse(locationAndProduct);
-        usedGroup = new GreedyQS().getLocations(productList, locationAndProduct);
-        return usedGroup;
+        locationAndProduct.sort(Comparator.comparingLong(ProductsLocations::getQuantity).reversed());
+        return new GreedyQS().getLocations(productList, locationAndProduct);
     }
 }
