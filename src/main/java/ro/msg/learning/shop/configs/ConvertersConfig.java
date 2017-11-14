9317c088db673bea6da3111881df71ce87d58df4
ZbRoby <robi1796@yahoo.com>
2017-11-14 14:53:16 +0200

    added configs and handlers packets and rename things accordingly

---
+++ src/main/java/ro/msg/learning/shop/configs/ConvertersConfig.java
@@ -0,0 +1,43 @@
+package ro.msg.learning.shop.configs;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import ro.msg.learning.shop.converter.CsvHttpMessageConverter;
+import ro.msg.learning.shop.entities.*;
+import ro.msg.learning.shop.mixins.*;
+import ro.msg.learning.shop.models.OrderInput;
+import ro.msg.learning.shop.models.ShelfProduct;
+
+/**
+ * @author Zbiera Alexandru-Robert <Robert.Zbiera@msg.group>
+ */
+
+@Configuration
+public class ConvertersConfig {
+
+    @Bean
+    public HttpMessageConverter customConverters() {
+        return new CsvHttpMessageConverter();
+    }
+
+    @Bean
+    public Module jsonModule() {
+        SimpleModule module = new SimpleModule();
+        module.setMixInAnnotation(Address.class, AddressMixin.class);
+        module.setMixInAnnotation(Customer.class, CustomerMixin.class);
+        module.setMixInAnnotation(Employee.class, EmployeeMixin.class);
+        module.setMixInAnnotation(Location.class, LocationMixin.class);
+        module.setMixInAnnotation(Order.class, OrderMixin.class);
+        module.setMixInAnnotation(OrderDetails.class, OrderDetailsMixin.class);
+        module.setMixInAnnotation(Product.class, ProductMixin.class);
+        module.setMixInAnnotation(ProductCategory.class, ProductCategoryMixin.class);
+        module.setMixInAnnotation(ProductDetails.class, ProductDetailsMixin.class);
+        module.setMixInAnnotation(ProductsLocations.class, ProductsLocationsMixin.class);
+        module.setMixInAnnotation(OrderInput.class, OrderInputMixin.class);
+        module.setMixInAnnotation(ShelfProduct.class, ShelfProductMixin.class);
+        return module;
+    }
+}
