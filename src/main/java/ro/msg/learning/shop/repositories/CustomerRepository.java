1378ea87167aff54f3f73aaf96e83ace99d4d591
ZbRoby <robi1796@yahoo.com>
2017-11-20 13:04:49 +0200

    added three new methods to CustomerRepository an EmployeeRepository

--- src/main/java/ro/msg/learning/shop/repositories/CustomerRepository.java
+++ src/main/java/ro/msg/learning/shop/repositories/CustomerRepository.java
@@ -3,6 +3,9 @@ package ro.msg.learning.shop.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ro.msg.learning.shop.entities.Customer;
+import ro.msg.learning.shop.entities.User;
+
+import java.util.Optional;
 
 /**
  * @author Zbiera Alexandru-Robert <Robert.Zbiera@msg.group>
@@ -10,4 +13,10 @@ import ro.msg.learning.shop.entities.Customer;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findOptionalByUser(User user);
+
+    Optional<Customer> findOptionalByUserUsername(String username);
+
+    Optional<Customer> findOptionalByUserId(long id);
 }
--- src/main/java/ro/msg/learning/shop/repositories/EmployeeRepository.java
+++ src/main/java/ro/msg/learning/shop/repositories/EmployeeRepository.java
@@ -3,6 +3,9 @@ package ro.msg.learning.shop.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ro.msg.learning.shop.entities.Employee;
+import ro.msg.learning.shop.entities.User;
+
+import java.util.Optional;
 
 /**
  * @author Zbiera Alexandru-Robert <Robert.Zbiera@msg.group>
@@ -10,4 +13,10 @@ import ro.msg.learning.shop.entities.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    Optional<Employee> findOptionalByUser(User user);
+
+    Optional<Employee> findOptionalByUserUsername(String username);
+
+    Optional<Employee> findOptionalByUserId(long id);
 }
